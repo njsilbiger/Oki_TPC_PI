0ff36de67483fcee717dd997a87598da218ee009
ashleycarreiro <acarreir@hawaii.edu>
2025-09-25 15:37:44 -1000

    New plots, scripts, added Temperature and Tide Data

---
+++ Scripts/SfMAnalysis.R
@@ -0,0 +1,11 @@
+#### SfM Photogrammetry Analysis #####
+### Created by: Ashley Carreiro
+### Updated on: 2025_09-24
+
+#Load Libraries ####
+library(tidyverse)
+library(here)
+library(devtools)
+devtools::install_github("jmadinlab/habtools") 
+library(habtools)
+library(raster)
--- Scripts/TiltMeterAnalysis.R
+++ Scripts/TiltMeterAnalysis.R
@@ -3,19 +3,14 @@
 library(tidyverse)
 library(here)
 library(climaemet)
-
-#### Load Data ###
-
-Tilt1Data <- read_csv(here("TiltMeterData", "Tilt1_CurrentData.csv"))
-view(Tilt1Data)
+library(rstatix)
 
 
+####Condensing Data IGNORE and continue from Load Data#####
+##Only overwrote the orginal CSV because it was too big to upload to GitHub 
 
-### Tilt Meter 7 Data Condensing ###
-##Tilt 7 was set to take a reading at every second we will use the lubridate function to condense it to one minute intervals
-
-Tilt7_CurrentData <- read_csv(here("TiltMeterData", "Tilt7_CurrentData.csv")) %>%
-
+Tilt7_CurrentData <- read_csv(here("TiltMeterData", "Tilt7_CurrentData.csv")) %>% #Was taking recordings every second
+  
   group_by(DateTime = floor_date(DateTime, "min")) %>% #New datetime that is rounded to the start of each hour
   summarise( #Averaging the remaining columns by hour
     Speed = mean(Speed, na.rm = TRUE), 
@@ -25,9 +20,42 @@ Tilt7_CurrentData <- read_csv(here("TiltMeterData", "Tilt7_CurrentData.csv")) %>
     .groups = "drop") #Give you a clean data frame and drops the groups
 
 
-write.csv(Tilt7_CurrentData,  file ="TiltMeterData/Tilt7_CurrentData.csv") #Overwriting the CSV file with the new condensed one
+write_csv(Tilt7_CurrentData,  file ="TiltMeterData/Tilt7_CurrentData.csv") #Overwriting the CSV file with the new condensed one
 
-## Tilt 8 was taking data every 20 seconds 
+
+### Tilt 4 was taking data every 20 seconds ###
+Tilt4_CurrentData <- read_csv(here("TiltMeterData", "Tilt4_CurrentData.csv")) %>%
+  
+  group_by(DateTime = floor_date(DateTime, "min")) %>% #New datetime that is rounded to the start of each hour
+  summarise( #Averaging the remaining columns by hour
+    Speed = mean(Speed, na.rm = TRUE), 
+    Heading = mean(Heading, na.rm = TRUE), 
+    Velocity_N = mean(Velocity_N, na.rm = TRUE), 
+    Velocity_E = mean(Velocity_E, na.rm = TRUE), 
+    .groups = "drop") #Give you a clean data frame and drops the groups
+
+write_csv(Tilt4_CurrentData,  file ="TiltMeterData/Tilt4_CurrentData_Condensed.csv") ##Writing a new CSV file
+
+##Importing Tide Data from .txt file and save filtered data for appropriate dates
+#https://www.gsi.go.jp/kanshi/tide_data_21_e.html
+
+TideData_txtFile <- "Data/TiltMeterData/21_2021-2025_hour.txt" #Reading in the txt file
+
+TideData1 <- read.table(TideData_txtFile, header = FALSE)
+view(TideData)
+
+colnames(TideData1) <- c("Number", "Date", "Hour", "Sealevel_mm", "DatumConstant_mm", "FixedPointHeight_mm") #Changing the column names
+head(TideData1)
+TideData1 <- TideData1[,-1] #Removing the first row which was #No from the data?
+head(TideData1)
+
+TideDataFiltered <- TideData1 %>% #Filtering out the dates that data was not recorded during 
+  filter(between(Date, "2025/08/01", "2025/08/15"))
+View(TideData1Filtered)
+
+write_csv(TideData1Filtered, here("Data/TiltMeterData/TideData_2025.08.csv"))
+
+### Tilt 8 was taking data every 20 seconds 
 Tilt8_CurrentData <- read_csv(here("TiltMeterData", "Tilt8_CurrentData.csv")) %>%
   
   group_by(DateTime = floor_date(DateTime, "min")) %>% #New datetime that is rounded to the start of each hour
@@ -38,40 +66,123 @@ Tilt8_CurrentData <- read_csv(here("TiltMeterData", "Tilt8_CurrentData.csv")) %>
     Velocity_E = mean(Velocity_E, na.rm = TRUE), 
     .groups = "drop") #Give you a clean data frame and drops the groups
 
+write_csv(Tilt8_CurrentData,  file ="TiltMeterData/Tilt8_CurrentData.csv")
 
-write.csv(Tilt8_CurrentData,  file ="TiltMeterData/Tilt8_CurrentData.csv")
+#### Load Data ####
+ #Tilt Meter Data
+Tilt1Data <- read_csv(here("Data", "TiltMeterData", "Tilt1_CurrentData.csv"))
+Tilt2Data <- read_csv(here("Data", "TiltMeterData", "Tilt2_CurrentData.csv"))
+Tilt3Data <- read_csv(here("Data","TiltMeterData", "Tilt3_CurrentData.csv"))
+Tilt4Data <- read_csv(here("Data","TiltMeterData", "Tilt4_CurrentData_Condensed.csv"))
+Tilt5Data <- read_csv(here("Data","TiltMeterData", "Tilt5_CurrentData.csv"))
+Tilt6Data <- read_csv(here("Data","TiltMeterData", "Tilt6_CurrentData.csv"))
+Tilt7Data <- read_csv(here("Data","TiltMeterData", "Tilt7_CurrentData.csv"))
+Tilt7Data <- Tilt7Data[,-1] # Had to remove a column that was created giving each entry a point
+Tilt8Data <- read_csv(here("Data","TiltMeterData", "Tilt8_CurrentData.csv"))
+Tilt8Data <- Tilt8Data[,-1]
+
+#Load Temperature Data
+
+Tilt1Temp <- read_csv(here("Data/TiltMeterData/Tilt1_TemperatureData.csv"))
+Tilt2Temp <- read_csv(here("Data/TiltMeterData/Tilt2_TemperatureData.csv"))
+Tilt3Temp <- read_csv(here("Data/TiltMeterData/Tilt3_TemperatureData.csv"))
+Tilt4Temp <- read_csv(here("Data/TiltMeterData/Tilt4_TemperatureData.csv"))
+Tilt5Temp <- read_csv(here("Data/TiltMeterData/Tilt5_TemperatureData.csv"))
+Tilt6Temp <- read_csv(here("Data/TiltMeterData/Tilt6_TemperatureData.csv"))
+Tilt7Temp <- read_csv(here("Data/TiltMeterData/Tilt7_TemperatureData.csv"))
+Tilt8Temp <- read_csv(here("Data/TiltMeterData/Tilt8_TemperatureData.csv"))
+
+#Load Tide Data, from Okinawa Tide Station 
+
+TideData <- read_csv(here("Data/TiltMeterData/TideData_2025.08.csv"))
+
+#### Organizing the Tilt Speed Data ####
+
+declination <- -5.85 # West declination in degrees to adjust magnetic north (which the sensors take) to true north
+#declination is the same for all sensors 
+
+Tilt_Data_fun <- function(df, tilt_name){  #Function to add a tilt name column and separate data and time column 
+df %>%
+    
+separate(col = DateTime, #Choose the column that you want separated
+             into = c("Date", "Time"), # Separate it into two columns "Data" and "Time"
+             sep = " ", #What are the two separated by 
+             remove = FALSE) %>% #Keep the orginial DateTime Column    
+    
+mutate(DateTime = lubridate::ymd_hms(DateTime), #Set datetime
+       Date = lubridate::ymd(Date), #Set Date
+       Time = lubridate::hms(Time), #Set Time
+       Speed_m = Speed * 0.01, #Change cm/s -> m/s
+       VelocityN_m = Velocity_N * 0.01, #Change cm/s -> m/s
+       VelocityE_m = Velocity_E* 0.01, #Change cm/s -> m/s
+       Heading_True = (Heading + declination) %% 360, #Adjusting magnetic north Heading to true north using the declination of the area
+       TiltMeter = tilt_name) %>%
+    
+    select(TiltMeter, everything()) #Moving TiltMeter# Column to be the first column
+    
+}
+
+Tilt1DataClean <- Tilt_Data_fun(Tilt1Data, "Tilt1")
+view(Tilt1DataClean)
+Tilt2DataClean <- Tilt_Data_fun(Tilt2Data, "Tilt2")
+Tilt3DataClean <- Tilt_Data_fun(Tilt3Data, "Tilt3")
+Tilt4DataClean <- Tilt_Data_fun(Tilt4Data, "Tilt4")
+Tilt5DataClean <- Tilt_Data_fun(Tilt5Data, "Tilt5")
+Tilt6DataClean <- Tilt_Data_fun(Tilt6Data, "Tilt6")
+Tilt7DataClean <- Tilt_Data_fun(Tilt7Data, "Tilt7")
+Tilt8DataClean <- Tilt_Data_fun(Tilt8Data, "Tilt8")
+
+####Summary Stats of all TiltMeter Speed Data ####
+
+JoinedData <- rbind(Tilt1DataClean, Tilt2DataClean, Tilt3DataClean, Tilt4DataClean, Tilt5DataClean,  Tilt6DataClean,  Tilt7DataClean, Tilt8DataClean)
+view(JoinedData) #Joined the data so that we can look at min and max values for all tilt meters
 
 
+## Visualizing the summary stats to see where to filter the data off at ##
 
-### Using Lubridate to set DateTime Column"
-
-tilt_fun<-function(Tilt4Data = Tilt4Data, plotname){ #Creating a function to run all tilt meter data easier
-
-  declination <- -5.85 # West declination in degrees to adjust magnetic north (which the sensors take) to true north
-  
-  Tilt4Data$DateTime <- ymd_hms(Tilt4Data$DateTime) #Setting the DateTime Column
-  Tilt4Data$Speed_m <- Tilt4Data$Speed * 0.01 #Converting cm/s to m/s
-  Tilt4Data$VelocityN_m <- Tilt4Data$Velocity_N * 0.01
-  Tilt4Data$VelocityE_m <- Tilt4Data$Velocity_E * 0.01
-  Tilt4Data$Heading_True <- (Tilt4Data$Heading + declination) %% 360 # Create a new column for True Heading
-  
-
-  
-  ### Making a windrose plot of the current data
+summary(JoinedData$Speed_m)
+is_outlier(JoinedData$Speed_m) %>%
+  table()
+lower_bound <- quantile(JoinedData$Speed_m, 0.001)
+lower_bound #0.1% of the data lower quantile number
+Upper_bound <- quantile(JoinedData$Speed_m, 0.999)
+Upper_bound #99.9% of the data upper bound limit (0.2501416 )
 
 
-  
-  ggwindrose(
-    speed = Tilt4Data$Speed_m, 
-    direction = Tilt4Data$Heading_True,
+boxplot.stats(JoinedData$Speed_m)
+
+sum(JoinedData$Speed_m >= .5) #Counting the number of points above .5
+# = 5
+
+sum(JoinedData$Speed_m > 0.2501416) #Number of points above the upper bound, 0.25, 145 points
+
+table(cut(JoinedData$Speed_m , seq(0, .25,.05), include.lowest = TRUE)) ##Showing me the different bins and how many data points I have in each one
+
+Means <- JoinedData %>%
+  group_by(TiltMeter) %>%
+  summarise(mean(Speed_m))
+Means #Giving the average speed m/s of each tilt meter
+
+#Visualize the data 
+
+ggplot(JoinedData, aes(x = DateTime, y = Speed_m)) +
+  geom_point() +
+   facet_wrap(~TiltMeter)
+
+
+### Making a windrose plot ####
+tilt_fun<-function(df, plotname){ #Creating a function to run all tilt meter data easier
+
+    ggwindrose(
+    speed = df$Speed_m, 
+    direction = df$Heading_True,
     n_directions = 8,
-    speed_cuts = seq(0, 0.50,.05), Inf) + 
+    speed_cuts = seq(0, 0.25,.05), Inf) + 
     #stack_revers = TRUE) + #If we want the higher speeds on the outside of the plot
-    
-    
+  
 
     scale_fill_manual(
-      values = c( "skyblue","blue", "cyan" ,"green","yellow", "tan", "darkorange1","hotpink","magenta", "red", "red4"),
+      values = c( "skyblue","orange" ,"green2", "blue","red",  "darkred"),
       drop = FALSE
     ) +
     
@@ -85,57 +196,77 @@ tilt_fun<-function(Tilt4Data = Tilt4Data, plotname){ #Creating a function to run
           #legend.box.background = element_rect(fill = "transparent", color = NA))
       
   
-  
-  
   ggsave(here("Output","TiltMeterData",plotname))
   
 }
 
-Tilt1Data<-read_csv(here("TiltMeterData","Tilt1_CurrentData.csv"))
-tilt_fun(Tilt1Data, plotname = "tilt1.png")
-
-
-Tilt2Data<-read_csv(here("TiltMeterData","Tilt2_CurrentData.csv"))
-tilt_fun(Tilt2Data, plotname = "tilt2.png")
-
-Tilt3Data <- read_csv(here("TiltMeterData", "Tilt3_CurrentData.csv"))
-
-
-tilt_fun(Tilt3Data, plotname = "tilt3.png")
-
-Tilt4Data <- read_csv(here("TiltMeterData", "Tilt4_CurrentData.csv"))
-tilt_fun(Tilt4Data, plotname = "tilt4.png")
-
-Tilt5Data <- read_csv(here("TiltMeterData", "Tilt5_CurrentData.csv"))
-
-tilt_fun(Tilt5Data, plotname = "tilt5.png")
-
-Tilt6Data <- read_csv(here("TiltMeterData", "Tilt6_CurrentData.csv"))
-tilt_fun(Tilt6Data, plotname = "tilt6.png")
-
-Tilt7Data <- read_csv(here("TiltMeterData", "Tilt7_CurrentData.csv"))
-tilt_fun(Tilt7Data, plotname = "tilt7.png")
-
-
-Tilt8Data <- read_csv(here("TiltMeterData", "Tilt8_CurrentData.csv"))
-tilt_fun(Tilt8Data, plotname = "tilt8.png")
-
-view(Tilt4Data)
-table(cut(Tilt4Data$Speed *0.01 , seq(0, .5,.05), include.lowest = TRUE)) ##Showing me the different bins and how many data points I have in each one
-
-mean(Tilt1Data$Speed) * 0.01
-mean(Tilt2Data$Speed) * 0.01
-mean(Tilt3Data$Speed) * 0.01
-mean(Tilt4Data$Speed) * 0.01
-mean(Tilt5Data$Speed) * 0.01
-mean(Tilt6Data$Speed) * 0.01
-mean(Tilt7Data$Speed) * 0.01
-mean(Tilt8Data$Speed) * 0.01
+tilt_fun(df= Tilt1DataClean, plotname = "tilt1b.jpeg") #Saving as jpeg to upload into googleEarth with a white background
+tilt_fun(Tilt2DataClean, plotname = "tilt2b.jpeg")
+tilt_fun(Tilt3DataClean, plotname = "tilt3b.jpeg")
+tilt_fun(Tilt4DataClean, plotname = "tilt4b.jpeg")
+tilt_fun(Tilt5DataClean, plotname = "tilt5b.jpeg")
+tilt_fun(Tilt6DataClean, plotname = "tilt6b.jpeg")
+tilt_fun(Tilt7DataClean, plotname = "tilt7b.jpeg")
+tilt_fun(Tilt8DataClean, plotname = "tilt8b.jpeg")
 
 
 
+#### Organizing Temperature Data ####
+Tilt_Temp_fun <- function(df){ 
+df %>%
+separate(col = DateTime, #Choose the column that you want separated
+         into = c("Date", "Time"), # Separate it into two columns "Data" and "Time"
+         sep = " ", #What are the two separated by 
+         remove = FALSE) %>% #Keep the orginial DateTime Column    
+  mutate(DateTime = lubridate::ymd_hms(DateTime), #Set datetime
+         Date = lubridate::ymd(Date), #Set Date
+         Time = lubridate::hms(Time))
+}
 
-#### The rest of the script is testing different plots ####
+Tilt1TempData <- Tilt_Temp_fun(Tilt1Temp)
+Tilt2TempData <- Tilt_Temp_fun(Tilt2Temp)
+Tilt3TempData <- Tilt_Temp_fun(Tilt3Temp)
+Tilt4TempData <- Tilt_Temp_fun(Tilt4Temp)
+Tilt5TempData <- Tilt_Temp_fun(Tilt5Temp)
+Tilt6TempData <- Tilt_Temp_fun(Tilt6Temp)
+Tilt7TempData <- Tilt_Temp_fun(Tilt7Temp)
+Tilt8TempData <- Tilt_Temp_fun(Tilt8Temp)
+
+####Plotting Temperature Data ####
+Tilt_TempPlot_fun <- function(df, plotname){ 
+
+  ggplot(df, aes(DateTime, Temperature_C)) +
+  geom_point()
+  
+
+ggsave(here("Output", "TiltMeterData", plotname))
+}
+
+Tilt_TempPlot_fun(Tilt1TempData, plotname = "Tilt1TempPlot.png")
+Tilt_TempPlot_fun(Tilt2TempData, plotname = "Tilt2TempPlot.png")
+Tilt_TempPlot_fun(Tilt3TempData, plotname = "Tilt3TempPlot.png")
+Tilt_TempPlot_fun(Tilt4TempData, plotname = "Tilt4TempPlot.png")
+Tilt_TempPlot_fun(Tilt5TempData, plotname = "Tilt5TempPlot.png")
+Tilt_TempPlot_fun(Tilt6TempData, plotname = "Tilt6TempPlot.png")
+Tilt_TempPlot_fun(Tilt7TempData, plotname = "Tilt7TempPlot.png")
+Tilt_TempPlot_fun(Tilt8TempData, plotname = "Tilt8TempPlot.png")
+
+
+#### Plotting Tide Data ####
+TideData <- TideData %>%
+  mutate(DateTime = ymd_hms(paste(Date, sprintf("%02d:00:00", Hour))), #Combining Data and time into one column 
+  Sealevel_m = Sealevel_mm / 1000) #Convert mm to m 
+view(TideData)
+
+
+ggplot(TideData, aes(DateTime, Sealevel_m)) +
+  geom_line()
+
+
+#CorrectedSealevel_mm = TideData$Sealevel_mm + TideData$DatumConstant_mm - TideData$FixedPointHeight_mm
+#(CorrectedSealevel_mm/1000)
+
+### The rest of the script is testing different plots ####
 
 # Pre-bin the data by direction + speed bins
 rose_data <- 
@@ -231,3 +362,4 @@ ggplot(Tilt1Binned, aes(x = dir_center, y = percent, fill = factor(spd_bin))) +
     axis.ticks = element_blank()
   ) +
   labs(title ="Tilt1 Current Flow Rose (1-min data)")
+
